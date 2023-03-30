d9e2331899be9af88bbeb2030215ddb74c6fe275
HarutyunyanLiana08 <harutyunyanliana08@gmail.com>
2023-03-30 12:32:01 +0400

    function

---
+++ src/AccordionFunc.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from "react"
+
+const AccordionFunc = ()=>{
+    const[countries, setCountries]=useState([])
+    const[activeIndex, setActiveIndex]=useState("")
+    const[showMore, setShowMore]=useState(false)
+
+    useEffect(()=>{
+        fetch("https://countriesnow.space/api/v0.1/countries/capital")
+        .then(res=>res.json())
+        .then(data=>setCountries(data.data))
+        .catch(error=>console.log(error))
+    },[])
+    return(
+        <div>
+            {countries.slice(0, showMore ? countries.length : 20).map((country, index)=>(
+                <div key={index}>
+                    <button className="button"onClick={()=>setActiveIndex((prevState =>prevState === index ? null:index))}>{country.name}+</button>
+                    {index === activeIndex && (
+                        <div>
+                        <p className="button"> Capital is: {country.capital}</p>
+                       
+                        </div>
+                    )}
+                </div>
+            ))}
+            <div>
+            <button onClick={() => setShowMore(!showMore)}>{showMore ? "Show Less" : "Show More"}</button>
+            </div>
+        </div>
+    ) 
+}
+
+export default AccordionFunc
--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import Accordion from './Accordion';
-
+import AccordionFunc from './AccordionFunc';
+import {Route, Link, Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       
-      <Accordion />
+      <h1>Components</h1>
+      <div>
+        <ul>
+          <li> <Link to="/class">ClassComponent</Link> </li>
+          <li> <Link to="/function">FunctionComponent</Link> </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path='/class' element={<Accordion />} />
+        <Route path='/function' element={<AccordionFunc />} />
+      </Routes>
     </div>
   );
 }
